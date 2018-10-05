671b4d0502716fa842ad860a307bd218f5d4c5f3
Tasfin <tareque20@gmail.com>
2018-10-05 17:33:50 +0600

    added counter and list using file system example

---
+++ list_using_filesystem/view.js
@@ -0,0 +1,51 @@
+let $ = require('jquery')
+let fs = require('fs')
+let filename = 'contacts'
+let sno = 0
+
+$('#add-to-list').on('click', () => {
+   let name = $('#Name').val()
+   let email = $('#Email').val()
+
+   //fs.appendFile('contacts', name + ',' + email + '\n')
+   fs.appendFile(filename, name + ',' + email + '\n', function (err) {
+	  if (err) {
+	  	console.log(err);
+	  	throw err;
+	  }
+	  console.log('Saved!');
+	});
+
+   addEntry(name, email)
+})
+
+function addEntry(name, email) {
+   if(name && email) {
+      sno++
+      let updateString = '<tr><td>'+ sno + '</td><td>'+ name +'</td><td>' 
+         + email +'</td></tr>'
+      $('#contact-table').append(updateString)
+   }
+}
+
+function loadAndDisplayContacts() {  
+   
+   //Check if file exists
+   if(fs.existsSync(filename)) {
+      let data = fs.readFileSync(filename, 'utf8').split('\n')
+      
+      data.forEach((contact, index) => {
+         let [ name, email ] = contact.split(',')
+         addEntry(name, email)
+      })
+   
+   } else {
+      console.log("File Doesn\'t Exist. Creating new file.")
+      fs.writeFile(filename, '', (err) => {
+         if(err)
+            console.log(err)
+      })
+   }
+}
+
+loadAndDisplayContacts()
